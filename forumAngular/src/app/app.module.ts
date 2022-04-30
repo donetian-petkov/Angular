fef8d645c5358f8d3097f5ae4e2e824e55040a93
donetian-petkov <donetian+petkov@gmail.com>
2022-04-30 21:51:35 +0300

    forumAngular

---
+++ forumAngular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import {CoreModule} from "./core/core.module";
+import { ThemeComponent } from './theme/theme.component';
+import {ContentService} from "./content.service";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ThemeComponent
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule
+  ],
+  providers: [ContentService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ forumAngular/src/app/content.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {IPost, ITheme} from "./shared/interfaces";
+import { environment } from '../environments/environment';
+
+const API_URL = environment.apiURL;
+
+@Injectable()
+export class ContentService {
+
+  constructor(private http: HttpClient) { }
+
+  loadThemes() {
+    return this.http.get<ITheme[]>(`${API_URL}/themes`)
+  }
+
+  loadPosts(limit?: number) {
+    const query = limit ? `?limit=${limit}` : '';
+    return this.http.get<IPost[]>(`${API_URL}/posts${query}`)
+
+  }
+}
